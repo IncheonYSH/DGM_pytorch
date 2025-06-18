246e44b940306224beb0abd27d4dd2b98e7158ab
IncheonYSH <ysh121095@gmail.com>
2025-06-18 11:46:04 +0900

    Modify: change .sh command

--- preprocess.sh
+++ preprocess.sh
@@ -2,14 +2,14 @@
 # Usage: bash preprocess.sh BASE_PATH METADATA_CSV SPLIT_CSV LABEL_CSV OUTPUT_PATH [MIMIC_CSV] [CHEXBERT_CKPT] [DEVICE]
 set -e
 
-BASE_PATH="$1"
-METADATA_CSV="$2"
-SPLIT_CSV="$3"
-LABEL_CSV="$4"
-OUTPUT_PATH="$5"
-MIMIC_CSV="${6:-}"
-CHEXBERT_CKPT="${7:-}"
-DEVICE="${8:-cpu}"
+BASE_PATH="/data/mimic-cxr-jpg/files"
+METADATA_CSV="/data/mimic-cxr-jpg/mimic-cxr-2.0.0-metadata.csv"
+SPLIT_CSV="/data/mimic-cxr-jpg/mimic-cxr-2.0.0-split.csv"
+LABEL_CSV="/data/mimic-cxr-jpg/mimic-cxr-2.0.0-chexpert.csv"
+OUTPUT_PATH="./data"
+MIMIC_CSV="/home/shyoon/rrg_ttc/forced_generation/mimic_forced_generation.csv"
+CHEXBERT_CKPT="/home/shyoon/CheXbert/chexbert.pth"
+DEVICE="gpu"
 
 python data_preprocess_all.py \
     --base_path "$BASE_PATH" \
--- train.sh
+++ train.sh
@@ -2,11 +2,11 @@
 # Usage: bash train.sh TRAIN_LIST VAL_LIST [BATCH_SIZE] [D_STEPS] [GPU]
 set -e
 
-TRAIN_LIST="$1"
-VAL_LIST="$2"
-BATCH_SIZE="${3:-64}"
-D_STEPS="${4:-1}"
-GPU="${5:-0}"
+TRAIN_LIST="./data/filtered_labeled_train_sd.txt"
+VAL_LIST="./data/filtered_labeled_validation_sd.txt"
+BATCH_SIZE=1
+D_STEPS=1
+GPU=3
 
 python train.py \
     --batch_size "$BATCH_SIZE" \
